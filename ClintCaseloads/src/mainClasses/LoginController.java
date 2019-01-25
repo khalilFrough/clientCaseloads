8b8dc578a1fe8b2e5775e7e9cd18116f1780a1dd
khalilfrough <khalilfrough@khalils-mbp.staff_nonsoe.ames>
2019-01-25 19:22:27 +1100

    Connected to Database and run the test, works fine

--- ClintCaseloads/src/mainClasses/LoginController.java
+++ ClintCaseloads/src/mainClasses/LoginController.java
@@ -1,6 +1,37 @@
 package mainClasses;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.*;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable  {
 	
+	LoginModel loginModel = new LoginModel();
+	@FXML
+	private Label dbStatus;
+	@FXML
+	private TextField userName;
+	@FXML 
+	private PasswordField password; 
+	@FXML
+	private ComboBox <option> comboBox;
+	@FXML
+	private Button login; 
+	
+	
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		if(this.loginModel.isDatabaseConnected()) {
+			this.dbStatus.setText("Connected to DB");
+		}else {
+			this.dbStatus.setText("Not Connected");
+		}
+		
+		
+		this.comboBox.setItems(FXCollections.observableArrayList(option.values()) );
+	}
 
 }
--- ClintCaseloads/src/mainClasses/LoginModel.java
+++ ClintCaseloads/src/mainClasses/LoginModel.java
@@ -1,6 +1,8 @@
 package mainClasses;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException; 
 import dbClass.SqliteDbClass;
 public class LoginModel {
@@ -18,6 +20,34 @@ public class LoginModel {
 		if(this.connection==null) {
 			System.exit(1);
 		}
+	}
+	public boolean isDatabaseConnected() {
+		return this.connection!= null; 
+	}
+	
+	public boolean isLogin(String user, String pass, String opt) {
+		PreparedStatement pr =null; 
+		ResultSet re= null; 
+		String sql= "SELECT * from login WHERE Username=? Password=? divistion=?";
+		
+		try {
+			
+			pr=this.connection.prepareStatement(sql);
+			pr.setString(1, user);
+			pr.setString(2, pass);
+			pr.setString(3, opt);
+			
+			re=pr.executeQuery();
+			
+			if(re.next()) {
+				return true;
+			}else
+				return false;
+		}catch(Exception ex) {
+			return false; 
+			
+		}
+		
 	}
 
 }
