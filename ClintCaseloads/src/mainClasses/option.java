5458ad7f55bf9acbf0e2159f200918e184e9dbac
khalilfrough <khalilfrough@khalils-mbp.gateway>
2019-01-30 19:42:05 +1100

    did some debugs and the final project seems to work.

--- ClintCaseloads/src/mainClasses/option.java
+++ ClintCaseloads/src/mainClasses/option.java
@@ -1,5 +1,5 @@
 package mainClasses;
-// this enum is created to create the two options CM and PSO for the applicaiton. 
+// this enum is created to create the two options CM and PSO for the application. 
 
 public enum option {
 	CM, PSO;
